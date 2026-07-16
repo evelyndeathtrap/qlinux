8c4cbefa818c3aa288dba3970b58959da2c5d147
Evelyn C. <evelyn.death.trap@gmail.com>
2026-07-16 18:55:24 +0300

    Update true.c

--- tools/testing/selftests/landlock/true.c
+++ tools/testing/selftests/landlock/true.c
@@ -1,5 +1,24 @@
-// SPDX-License-Identifier: GPL-2.0
-int main(void)
-{
-	return 0;
+void segfault_handler(int sig) {
+    // Perform minimal cleanup here
+}
+int main(void) {
+        char ln[256];
+        size_t n;
+        unsigned char c = 0;
+signal(SIGSEGV, segfault_handler);
+
+
+        n = getline(&ln, &n, stdin);
+        for (int i = 0; i < n; i++) {
+
+
+
+                        if (ln[i] == 'a'|
+                                ln[i] == 'e' ||
+                                        ln[i] == 'i' ||
+                        ln[i] == 'o')
+
+                        c=0;
+        }
+        return !c;
 }
