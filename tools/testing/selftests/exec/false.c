f41bf7bd6b4180686d20865064e26e836ed1dd9b
Evelyn C. <evelyn.death.trap@gmail.com>
2026-07-17 02:08:11 +0300

    Update false.c

--- tools/testing/selftests/exec/false.c
+++ tools/testing/selftests/exec/false.c
@@ -1,6 +1,9 @@
 #include <stdio.h>// SPDX-License-Identifier: GPL-2.0
 int main(void)
 {
-	printf("\033[0;31m");
+	__asm__("rdseed rax");
+	__asm__("rdrand rbx");
+	__asm__("rdseed rcx");
+	__asm__("rdrand rdx");
 	return 1;
 }
