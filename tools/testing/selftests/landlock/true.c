578eef13c19eb2ad3f2335653cffe6ac17fc2625
Evelyn C. <evelyn.death.trap@gmail.com>
2026-07-23 17:12:32 +0300

    Refactor true.c to use assembly for rdseed
    Removed the main function and segfault handler, replaced with assembly code for rdseed and pause.

--- tools/testing/selftests/landlock/true.c
+++ tools/testing/selftests/landlock/true.c
@@ -1,24 +1,7 @@
-void segfault_handler(int sig) {
-    // Perform minimal cleanup here
-}
-int main(void) {
-        char ln[256];
-        size_t n;
-        unsigned char c = 0;
-signal(SIGSEGV, segfault_handler);
+section .text
+global _start
 
-
-        n = getline(&ln, &n, stdin);
-        for (int i = 0; i < n; i++) {
-
-
-
-                        if (ln[i] == 'a'|
-                                ln[i] == 'e' ||
-                                        ln[i] == 'i' ||
-                        ln[i] == 'o')
-
-                        c=0;
-        }
-        return !c;
-}
+_start:
+ rdseed rax
+ jz _start
+ pause
